ea57778e8531119f75f50e442f65f2efc868e592
kj5210 <leclair6@uwindsor.ca>
2024-03-26 00:19:48 -0400

    Added GUI for activity_create_trip.xml and added to CreateTripActivity

--- app/src/main/java/com/example/goglobe/CreateTripActivity.java
+++ app/src/main/java/com/example/goglobe/CreateTripActivity.java
@@ -1,6 +1,10 @@
 package com.example.goglobe;
 
 import android.os.Bundle;
+import android.app.DatePickerDialog;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +12,40 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+
+import java.util.Calendar;
+
 public class CreateTripActivity extends AppCompatActivity {
+    private EditText editTextStartDate;
+    private EditText editTextEndDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_create_trip);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+            return WindowInsetsCompat.CONSUMED;
         });
+
+        editTextStartDate = findViewById(R.id.editTextStartDate);
+        editTextEndDate = findViewById(R.id.editTextEndDate);
+        editTextStartDate.setOnClickListener(view -> showDatePickerDialog(editTextStartDate));
+        editTextEndDate.setOnClickListener(view -> showDatePickerDialog(editTextEndDate));
+    }
+
+
+        private void showDatePickerDialog(final EditText dateField) {
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(CreateTripActivity.this,
+                (view, year1, monthOfYear, dayOfMonth) -> dateField.setText(String.format("%d-%02d-%02d", year1, monthOfYear + 1, dayOfMonth)), year, month, day);
+        datePickerDialog.show();
     }
 }

9f2ad7497eedf2156ed25d020a6aa04d8c7f969e
kj5210 <leclair6@uwindsor.ca>
2024-03-27 00:21:39 -0400

    Impletmented Create Trip Activity

--- app/src/main/java/com/example/goglobe/DataBaseHelper.java
+++ app/src/main/java/com/example/goglobe/DataBaseHelper.java
@@ -13,6 +13,13 @@ public class DataBaseHelper extends SQLiteOpenHelper {
     public static final String COLUMN_USER_USERNAME = "USER_USERNAME";
     public static final String COLUMN_USER_PASSWORD = "USER_PASSWORD";
     public static final String COLUMN_ID = "ID";
+    public static final String TRIP_TABLE = "TRIP_TABLE";
+    public static final String COLUMN_TRIP_ID = "TRIP_ID";
+    public static final String COLUMN_TRIP_USER_ID = "USER_ID";
+    public static final String COLUMN_TRIP_NAME = "NAME";
+    public static final String COLUMN_TRIP_DESTINATION = "DESTINATION";
+    public static final String COLUMN_TRIP_START_DATE = "START_DATE";
+    public static final String COLUMN_TRIP_END_DATE = "END_DATE";
 
     public DataBaseHelper(@Nullable Context context) {
         super(context, "goglobe.db", null, 1);
@@ -22,6 +29,9 @@ public class DataBaseHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
         String createTableStatement = "CREATE TABLE " + USER_TABLE + " (" + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + COLUMN_USER_USERNAME + " TEXT, " + COLUMN_USER_PASSWORD + " TEXT)";
         db.execSQL(createTableStatement);
+
+        String createTripTableStatement = "CREATE TABLE " + TRIP_TABLE + " (" + COLUMN_TRIP_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + COLUMN_TRIP_USER_ID + " INTEGER, " + COLUMN_TRIP_NAME + " TEXT, " + COLUMN_TRIP_DESTINATION + " TEXT, " + COLUMN_TRIP_START_DATE + " TEXT, " + COLUMN_TRIP_END_DATE + " TEXT, FOREIGN KEY(" + COLUMN_TRIP_USER_ID + ") REFERENCES " + USER_TABLE + "(" + COLUMN_ID + "))";
+        db.execSQL(createTripTableStatement);
     }
 
     @Override
@@ -64,6 +74,34 @@ public class DataBaseHelper extends SQLiteOpenHelper {
         cursor.close();
         db.close();
         return isValid;
+    }
+
+    public boolean addTrip(int userId, String name, String destination, String startDate, String endDate) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put(COLUMN_TRIP_USER_ID, userId);
+        cv.put(COLUMN_TRIP_NAME, name);
+        cv.put(COLUMN_TRIP_DESTINATION, destination);
+        cv.put(COLUMN_TRIP_START_DATE, startDate); // Store date as string
+        cv.put(COLUMN_TRIP_END_DATE, endDate); // Store date as string
+
+        long insert = db.insert(TRIP_TABLE, null, cv);
+        db.close();
+        return insert != -1;
+    }
+
+    public int getUserId(String username) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        String query = "SELECT " + COLUMN_ID + " FROM " + USER_TABLE + " WHERE " + COLUMN_USER_USERNAME + " = ?";
+        Cursor cursor = db.rawQuery(query, new String[]{username});
+
+        int id = -1; // ID not found
+        if (cursor.moveToFirst()) {
+            id = cursor.getInt(0); // Get ID
+        }
+        cursor.close();
+        db.close();
+        return id;
     }
 
 }
